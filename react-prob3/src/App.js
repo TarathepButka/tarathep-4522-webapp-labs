5fa047a9f9d450684384c61c85b11aee5b08daad
Fongt <tarathep.b@kkumail.com>
2024-10-04 12:01:29 +0700

    outputinpublic

--- react-prob3/src/App.js
+++ react-prob3/src/App.js
@@ -1,25 +1,8 @@
 import "./App.css";
+import { users } from "./users.js";
 import { GitHubInfo } from "./github_info";
 
 export default function App() {
-  const users = [
-    {
-      url: "https://github.com/TarathepButka",
-      imgURL: "https://avatars.githubusercontent.com/u/119283428?v=4",
-      alt: "Tarathep Butka",
-    },
-    {
-      url: "https://github.com/facebook/react",
-      imgURL: "https://avatars.githubusercontent.com/u/69631?s=48&v=4",
-      alt: "react",
-    },
-    {
-      url: "https://github.com/vercel/next.js",
-      imgURL: "https://avatars.githubusercontent.com/u/14985020?s=48&v=4",
-      alt: "next.js",
-    },
-  ];
-
   return (
     <div className="App">
       <GitHubInfo userInfo={users[0]} />
---
+++ react-prob3/src/users.js
@@ -0,0 +1,17 @@
+export const users = [
+    {
+      url: "https://github.com/TarathepButka",
+      imgURL: "https://avatars.githubusercontent.com/u/119283428?v=4",
+      alt: "Tarathep Butka",
+    },
+    {
+      url: "https://github.com/facebook/react",
+      imgURL: "https://avatars.githubusercontent.com/u/69631?s=48&v=4",
+      alt: "react",
+    },
+    {
+      url: "https://github.com/vercel/next.js",
+      imgURL: "https://avatars.githubusercontent.com/u/14985020?s=48&v=4",
+      alt: "next.js",
+    },
+  ];
